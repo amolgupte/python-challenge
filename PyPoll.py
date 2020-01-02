e2ce1b3dc47cec3cbe5a280be90d3c4fa2c3ef26
amolgupte <54489871+amolgupte@users.noreply.github.com>
2020-01-01 23:45:02 -0700

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,4 @@
+import os
+import csv
+os.chdir("C:/Users/amolg/OneDrive/Desktop/DU-DEN-DATA-PT-11-2019-U-C/02-Homework/03-Python/Instructions/PyPoll/Resources")
+csvpath = os.path.join("..", 'Resources', 'election_data.csv')
