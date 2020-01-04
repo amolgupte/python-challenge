c6ae25e167f19aa4cd1d8eb6d0c4f4e8ef08a14f
amolgupte <54489871+amolgupte@users.noreply.github.com>
2020-01-04 14:10:13 -0700

    Add files via upload

--- PyPoll.py
+++ PyPoll.py
@@ -5,13 +5,33 @@ csvpath = os.path.join("..", 'Resources', 'election_data.csv')
 with open(csvpath, newline='') as csvfile:
      csvreader = csv.reader(csvfile)
      print(csvreader)
+     
      csv_header = next(csvreader)
      print(f"CSV Header: {csv_header}")
+     
+       
      count=0
+     countKhan = 0
+     countCorrey = 0
+     countLi = 0
+     countOTooley = 0
      for row in csvreader:
-         count=count+1
+        count=count+1
+        if (row[2] == "Khan"):
+            countKhan = countKhan+1
+        elif(row[2] == "Correy"):
+            countCorrey=countCorrey+1
+        elif(row[2] =="Li"):
+            countLi=countLi+1
+        elif(row[2]=="O'Tooley"):
+            countOTooley=countOTooley+1
+
      print("Election Results ")
      print("-------------------------------")
      print("Total Votes: "+ str(count))
      print("--------------------------------")
-     
+     print("Khan " +str(countKhan))
+     print("Correy " + str(countCorrey))
+     print("Li " + str(countLi))
+     print("O'Tooley "+ str(countOTooley))
+     print("-------------------------------")
